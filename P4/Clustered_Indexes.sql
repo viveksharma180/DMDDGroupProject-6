89152ff4172a6e5dcf5bb4ca2b0812344f505ffb
viveksharma180 <sharma.viv@northeastern.edu>
2022-04-19 13:05:03 -0400

    Added non-clustered indexes.
    Signed-off-by: viveksharma180 <sharma.viv@northeastern.edu>

---
+++ P4/Clustered_Indexes.sql
@@ -0,0 +1,10 @@
+CREATE INDEX idx_BloodBankName
+ON BloodBank (blood_bank_name);
+
+
+CREATE INDEX idx_OrgName
+ON Organization (org_name);
+
+
+CREATE INDEX idx_HospitalName
+ON Hospital (hospital_name);
